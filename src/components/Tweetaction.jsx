c427c485aa5140686deba3ce4cd4f51bdf805f97
Rebecca <beccameme02@gmail.com>
2024-01-19 10:58:35 +0100

    Mises a jours du css dans les composantes LeftSidebar

--- src/components/Tweetaction.jsx
+++ src/components/Tweetaction.jsx
@@ -9,6 +9,7 @@ function TweetAction (props) {
   const handleClick = (action) =>{
     setActiveAction(action);
   };
+ 
   return(
     <div className="flex items-center justify-between px-4 py-2 space-x-8">
       <span className={`flex items-center cursor-pointer ${
@@ -16,15 +17,15 @@ function TweetAction (props) {
       }`}
       onClick={() => handleClick("reply")}>
       
-      <img src= {Reply} alt="Reply" className="w-4 mr-2 bg-blue-200  hover:scale-110 hover:opacity-75 "/>
+      <img src= {Reply} alt="Reply" className="w-4 mr-2 bg-blue-200  hover:scale-110 hover:opacity-50 "/>
       {props.comment}
       </span>
-      <span className={`flex items-center cursor-pointer ${
+  <span className={`flex items-center cursor-pointer ${
         activeAction === "retweet" ? "text-green-500 ": ""
       }`}
 onClick={() => handleClick("retweet")}>
       
-      <img src= {Groupe} alt="Retweet" className="w-4 mr-2 bg-green-200  hover:scale-110 hover:opacity-75 "/>
+      <img src= {Groupe} alt="Retweet" className="w-4 mr-2 bg-green-200  hover:scale-110 hover:opacity-50 "/>
       {props.retweet}
       </span>
       <span className={`flex items-center cursor-pointer ${
@@ -32,7 +33,7 @@ onClick={() => handleClick("retweet")}>
       }`}
       onClick={() => handleClick("react")}>
       
-      <img src= {ReactImage} alt="React" className="w-4 mr-2 bg-yellow-200  hover:scale-110 hover:opacity-75 "/>
+      <img src= {ReactImage} alt="React" className="w-4 mr-2 bg-yellow-200  hover:scale-110 hover:opacity-50 "/>
       {props.react}
 
       </span>
@@ -42,31 +43,6 @@ onClick={() => handleClick("retweet")}>
 
     </div>
   );
-  //  const [color1,setColor1] = useState("");
-  // const [color2,setColor2] = useState("");
-  // const [color3,setColor3] = useState("");
-  // const handleClick1 = ()=>{ setColor1("red")}
-  // const handleClick2 = ()=>{ setColor2("blue")}
-  // const handleClick3 = ()=>{ setColor3("yellow")}
-  // return (
-  //   <div className="tweet-actions">
-  //     <span className="tweet-action"onClick={handleClick1} style={{color: color1}}>
-  //       <img src={Reply} alt="" />
-  //      {props.comment} 
-  //     </span>
-  //     <span className="tweet-action" onClick={handleClick2} style={{color: color2}} >
-  //       <img src={Groupe} alt="" />
-  //       {props.retweet}
-  //     </span>
-  //     <span className="tweet-action" onClick={handleClick3} style={{color: color3}}>
-  //       { <img src={ReactImage} alt="" /> }
-        
-  //       {props.react}
-  //     </span>
-  //     <span className="tweet-action">
-  //       <img src={Share} alt="" />
-  //     </span>
-  //   </div>
-  // )
 }
 export default TweetAction;
+
